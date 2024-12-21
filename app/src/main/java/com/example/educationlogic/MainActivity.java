f774ba90e6ca22a0859b3b1238be7565bd68941f
river0203 <aspp3666@gmail.com>
2024-12-21 15:52:04 +0900

    feat : Plus play ground xml, class

--- app/src/main/java/com/example/educationlogic/MainActivity.java
+++ app/src/main/java/com/example/educationlogic/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.educationlogic;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button btnMoveplay = (Button) findViewById(R.id.button);
+
+        btnMoveplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), PlayGroundActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
