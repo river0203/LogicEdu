e12a1cf771c91c1e1301895385a13046513636d5
river0203 <aspp3666@gmail.com>
2024-12-22 05:14:02 +0900

    feat : Interface basic Logic
    회로의 기본적인 기능 및 상수

---
+++ app/src/main/java/com/example/educationlogic/LogicBasic.java
@@ -0,0 +1,9 @@
+package com.example.educationlogic;
+
+public interface LogicBasic {
+    // 부품의 틀
+
+    public static final boolean powerOn = true;
+
+    public abstract boolean checkCorrectPole(int breadBoardPointX, int breadBoardPointY);
+}
